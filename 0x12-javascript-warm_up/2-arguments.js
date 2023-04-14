ac879a570bbbbc5df23bd123e498e28f2baf4158
divineasset <blessingaladi@gmail.com>
2023-04-14 07:38:32 -0700

    done with the task

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
+const argc = process.argv.length;
 
-const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(myVar);
+if (argc > 2) {
+  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
+} else {
+  console.log('No argument');
+}
